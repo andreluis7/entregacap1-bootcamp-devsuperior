bca5a0fac15b5bf8ab22eb980a2fa6ff2d047cee
Andre Luis <adrluis7@gmail.com>
2020-10-04 17:41:49 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/andre/dsclient/services/ClientService.java
+++ backend/src/main/java/com/andre/dsclient/services/ClientService.java
@@ -1,7 +1,9 @@
 package com.andre.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.andre.dsclient.dto.ClientDTO;
 import com.andre.dsclient.entities.Client;
 import com.andre.dsclient.repositories.ClientRepository;
+import com.andre.dsclient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,5 +25,12 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity)
+;	}
 
 }
