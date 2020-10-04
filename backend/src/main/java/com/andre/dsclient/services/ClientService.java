2fecd3dcc863fecf676eaadb21211697b5e62dd6
Andre Luis <adrluis7@gmail.com>
2020-10-04 17:26:42 -0300

    DTO

--- backend/src/main/java/com/andre/dsclient/services/ClientService.java
+++ backend/src/main/java/com/andre/dsclient/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.andre.dsclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.andre.dsclient.dto.ClientDTO;
 import com.andre.dsclient.entities.Client;
 import com.andre.dsclient.repositories.ClientRepository;
 
@@ -16,8 +18,9 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 
 }
